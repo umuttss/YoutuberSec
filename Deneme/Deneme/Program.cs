9754b2665d8d7685b3e049e2fe22a7cc05e96151
Krozss <102265586+Krozss@users.noreply.github.com>
2023-09-05 19:51:16 +0300

    Add files via upload

---
+++ Deneme/Deneme/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Youtuber Seç, Kayıtlı İsimler,chinatsu, skinnydev, buben, ka6an, habip, hasan"); 
+
+string isim = Console.ReadLine();
+
+if (isim == "chinatsu")
+{
+    Console.WriteLine("İyi Seçim");
+}
+
+if (isim == "skinnydev")
+{
+    Console.WriteLine("Unity'i İyi Kullanıyor");
+}
+
+if (isim == "buben")
+{
+    Console.WriteLine("ALACA?");
+}
+
+if (isim == "ka6an")
+{
+    Console.WriteLine("Dunk'ı Yapan Adam");
+}
+
+if (isim == "marcus7z")
+{
+    Console.WriteLine("C3 Kullanan Adam");
+}
+
+if (isim == "habip")
+{
+    Console.WriteLine("Eyes of War?");
+}
+
+if (isim == "hasan")
+{
+    Console.WriteLine("Play Late Night Drive on Steam");
+}
+
+
+Console.ReadLine();
+
